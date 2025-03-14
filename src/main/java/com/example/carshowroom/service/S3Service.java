61d308f85de31fbbfb9229713de0b9686c6c9506
AsanteGyabengSamuel <samuel.asante@amalitech.com>
2025-03-14 16:07:48 +0000

    new s3 update

--- src/main/java/com/example/carshowroom/service/S3Service.java
+++ src/main/java/com/example/carshowroom/service/S3Service.java
@@ -1,9 +1,7 @@
 package com.example.carshowroom.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -16,33 +14,20 @@ import java.util.stream.Collectors;
 public class S3Service {
 
     private final S3Client s3Client;
+    private static final String BUCKET_NAME = "carsroombucket"; // Set your bucket name here
+    private static final String AWS_REGION = "us-east-2"; // Set your default region here
 
-    /*
-     * Setting up the AWS S3 connection
-     */
-    @Value("${AWS_BUCKETNAME}")
-    private String bucketName;
-
-    @Value("${AWS_REGION}")
-    private String region; // Added this so we can reuse it
-
-    public S3Service(@Value("${AWS_REGION}") String region,
-                     @Value("${secrets.AWS_ACCESS_KEY_ID}") String accessKey,
-                     @Value("${secrets.AWS_SECRET_ACCESS_KEY}") String secretKey) {
-
+    public S3Service() {
         this.s3Client = S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .region(Region.of(AWS_REGION))
+                .credentialsProvider(DefaultCredentialsProvider.create())
                 .build();
     }
 
-    /*
-     * Uploading a file to the S3 bucket
-     */
     public String uploadFile(String fileName, byte[] fileData) {
         try {
             s3Client.putObject(PutObjectRequest.builder()
-                            .bucket(bucketName)
+                            .bucket(BUCKET_NAME)
                             .key(fileName)
                             .build(),
                     RequestBody.fromBytes(fileData));
@@ -53,18 +38,14 @@ public class S3Service {
         }
     }
 
-    /*
-     * Listing files from the S3 bucket and returning their public URLs
-     */
     public List<String> listFiles() {
         try {
             ListObjectsV2Response response = s3Client.listObjectsV2(ListObjectsV2Request.builder()
-                    .bucket(bucketName)
+                    .bucket(BUCKET_NAME)
                     .build());
 
-            // Constructing the S3 URL for each object
             return response.contents().stream()
-                    .map(s3Object -> "https://" + bucketName + ".s3." + region + ".amazonaws.com/" + s3Object.key())
+                    .map(s3Object -> "https://" + BUCKET_NAME + ".s3." + AWS_REGION + ".amazonaws.com/" + s3Object.key())
                     .collect(Collectors.toList());
         } catch (S3Exception e) {
             throw new RuntimeException("Failed to list files: " + e.getMessage());
